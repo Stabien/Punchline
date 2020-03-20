7bdbab4bf8e8c96057a6442bbaf98e5420142302
Stabien <bastien.piedallu@gmail.com>
2020-03-20 15:48:56 +0100

    nouveau component search

--- MoviesAndMe/App.js
+++ MoviesAndMe/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search.js'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        /*<View style={styles.container}>
+            <Text>Coucou !</Text>
+        </View>*/
+        <Search/>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
